3a26186469524f944ab3a37d4d116bbb8dc18f55
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-11-27 14:34:38 +0600

    implemented hero home page hero section design

--- home/src/components/Hero/Hero.jsx
+++ home/src/components/Hero/Hero.jsx
@@ -1,25 +1,41 @@
+import { CiSearch } from "react-icons/ci";
+
 import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={styles["hero-container"]}>
       <div className={styles["hero-content"]}>
-        <h1 className={styles["hero-heading"]}>
-          Looking for a Business Solution for appointments?
-        </h1>
-        <p>
-          Ambel simplifies business management by providing tools for scheduling
-          appointments, managing bookkeeping, accepting payments, sharing
-          resources with customers, sending reminders, and running email
-          marketing campaigns.
-        </p>
-        <div className={styles["hero-cta"]}>
-          <button>Start A Free Trial</button>
-          <button>Find Professionals</button>
+        <div className={styles["hero-header"]}>
+          <h1 className={styles["hero-heading-main"]}>
+            Looking for a <span>Business Solution</span>
+            <br />
+            <span>for</span> appointments?
+          </h1>
+          <p className={styles["hero-para-main"]}>
+            Ambel simplifies business management by providing tools for
+            scheduling appointments, managing bookkeeping, accepting payments,
+            sharing resources with customers, sending reminders, and running
+            email marketing campaigns.
+          </p>
+          <div className={styles["hero-cta"]}>
+            <button className={styles["hero-cta-btn-trail"]}>
+              Start A Free Trial
+            </button>
+            <button className={styles["hero-cta-btn-find"]}>
+              <CiSearch />
+              <span>Find Professionals</span>
+            </button>
+          </div>
         </div>
-        <div></div>
+
+        <figure className={styles["hero-banner"]}>
+          <img
+            src="https://ambel.ca/_next/image?url=%2FhomePage%2Fhero%2FMain-image.webp&w=1200&q=75"
+            alt="Hero image of amble"
+          />
+        </figure>
       </div>
-      <figure></figure>
     </section>
   );
 };
