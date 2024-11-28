a9ea916d77afbab2275c3564ec087e162553d42a
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-11-28 14:41:43 +0600

    handled not found route on pricing app

--- pricing/src/App.jsx
+++ pricing/src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
 import "./index.scss";
 
 import Pricing from "./Pricing";
 
 const router = createBrowserRouter([
-  // { path: "/", element: <Home /> },
   { path: "/pricing", element: <Pricing /> },
+  {
+    path: "*",
+    element: (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <h2 className="text-center text-4xl font-semibold mb-4">
+            Page Not Found
+          </h2>
+          <Link
+            to="/pricing"
+            className="px-6 py-4 bg-[#19525A] rounded-2xl text-white"
+          >
+            Pricing Page
+          </Link>
+        </div>
+      </div>
+    ),
+  },
 ]);
 
 const App = () => {
